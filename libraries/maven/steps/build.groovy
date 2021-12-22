3f50ea4130d198986d7565c387c5551cea73ade0
hungcq83 <hungcq83@gmail.com>
2021-12-22 23:38:23 +0700

    Initial commit

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,5 +1,11 @@
 void call(){
     stage("Maven: Build"){
         println "build from the maven library"
+        def process = "find / -type d -name 'maven'".execute()
+        print "Output: " + process.text
+        print "Exit code: " + process.exitValue()
+        process = "pwd".execute()
+        print "Output: " + process.text
+        print "Exit code: " + process.exitValue()
     }
 }
--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,5 +1,11 @@
 void call(){
     stage("SonarQube: Static Code Analysis"){
         println "static code analysis from the sonarqube library"
+        def process = "find / -type d -name 'maven'".execute()
+        print "Output: " + process.text
+        print "Exit code: " + process.exitValue()
+        process = "pwd".execute()
+        print "Output: " + process.text
+        print "Exit code: " + process.exitValue()
     }
 }
